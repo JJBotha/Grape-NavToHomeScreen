7e8e80cb07c7d7af26775b316584bd27ecbcbb77
nethzor <dylanheunis11@gmail.com>
2019-03-05 14:06:22 +0200

    Navigate to screen block, 2 traits including button to open screen selection popup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//search 'implement' for outsystems sections
 import loadComponents from './components';
 import loadBlocks from './blocks';
 
@@ -5,19 +6,77 @@ export default (editor, opts = {}) => {
   const options = { ...{
     // default options
   },  ...opts };
+	var blockManager = editor.BlockManager;
+	var domComps = editor.DomComponents;
+	var dType = domComps.getType('default');
+	var dModel = dType.model;
+	var dView = dType.view;
+	
+	// Add components
+	loadComponents(editor, options);
 
-  // Add components
-  loadComponents(editor, options);
-
-  // Add blocks
-  //
-  var blockManager = editor.BlockManager;
-	blockManager.add('my-first-block', {
-	  label: 'Simple block',
-	  content: '<div class="my-block">This is a simple block</div>',
+	// Add navigation block
+	blockManager.add('NAVIGATE_SCREEN', {
+		id: 'initiateJob',
+		label: 'Navigate to screen',
+		content:{
+			content:'<div class="my-block fa fa-paint-brush">screen name</div>',
+			type:'NAVIGATE_SCREEN'
+		},
 	});
+	
+	//component traits
+	domComps.addType('NAVIGATE_SCREEN', {
+		model: dModel.extend({
+			defaults: Object.assign({}, dModel.prototype.defaults, {
+				traits: [
+					{
+						type: 'screenName',
+						label: 'Screen name',
+					},
+					{
+						type: 'screenSelectorButton',
+						label: '',
+						name: ''
+					},		  
+				], 
+			}),
+		}, {
+		  isComponent: function(el) {
+			if(el.tagName == 'INPUT'){
+			  return {type: 'input'};
+			}
+		  },
+		}),
+
+		view: dView,
+	});
+	
+	editor.TraitManager.addType('screenSelectorButton', {
+		events: {
+			'click': 'selectScreen' , // trigger parent onclick method on click
+		},
+		
+		selectScreen: function(){
+			var flowTemplate = prompt("screen name: ");
+			//implement
+			//$('#"+ navigateToScreenButton.Id +"').trigger('click');
+			//implement - after selection update trait value:
+			const traitEl = document.querySelector('input[type="screenName"]');	//implement these 2 lines in outsystems javascript after screen selection popup is closed.
+			traitEl && grapesjs.$(traitEl).val(flowTemplate).trigger('change');	
+		},
+		getInputEl() {
+            var button = document.createElement('button');
+            button.id = 'btnScreenSelector';
+			button.classList.add('actionButton');
+            button.appendChild(document.createTextNode('Select a screen'));
+			
+            return button;
+		
+		},
+	});
+	
+	
   loadBlocks(editor, options);
 
-  // TODO Remove
-  editor.on('load', () => editor.addComponents(`<div style="margin:100px; padding:25px;">Content loaded from the plugin</div>`, { at: 0 }))
 };
